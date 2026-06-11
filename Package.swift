0d1091adfb82b15dd0003b167a670231b9325ce8
The iOS Team @ GeneXus <builder@genexus.com>
2026-06-11 11:43:24 -0400

    Package version: 4.2.0-rc.20

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXUCMapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "4.2.0-rc.13"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-rc.13")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "4.2.0-rc.20"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-rc.20")
 	],
 	targets: [
 		.target(name: "GXUCMapsWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCMaps",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXUCMaps-4.2.0-rc.13.xcframework.zip",
-			checksum: "9d4a34071048080e2a13f77d2eb4ebfa4f14faeaecc7ba2c04b7d17289756df0"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXUCMaps-4.2.0-rc.20.xcframework.zip",
+			checksum: "1f69857ab23684f4fbe8e3ff4c8c39c26cc43f365af268ace724c9f3d6f4e859"
 		)
 	]
 )
